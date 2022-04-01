5dc695712db21aa6060c0361f171de34eb346e1f
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2022-04-01 10:37:27 +0200

    Cambios que aun no se si funcionan, pero la build se copia

--- build.bat
+++ build.bat
@@ -5,15 +5,19 @@ title Game_Build
 :: Directory variables
 set GAME_WORKING_DIR=%cd%
 set BUILD_DIR=%GAME_WORKING_DIR%\build\
-set ENGINE_DIR=%GAME_WORKING_DIR%\engine\K_Engine\
+set ENGINE_DIR=%GAME_WORKING_DIR%\K_Engine\
 
 :: Build all engine related stuff
+echo Building engine...
 cd %ENGINE_DIR%
 call build.bat
 cd %GAME_WORKING_DIR%
+echo Engine built
 
-:: Move all enigne files to the build folder
+:: Move all engine files to the build folder
 :: (Eventually we will move only the release ones)
+echo Copying engine files to game build folder...
 copy %ENGINE_DIR%\exe\*.exe %BUILD_DIR% 1>nul
 copy %ENGINE_DIR%\exe\*.dll %BUILD_DIR% 1>nul
 copy %ENGINE_DIR%\exe\*.cfg %BUILD_DIR% 1>nul
+echo Engine files copied
