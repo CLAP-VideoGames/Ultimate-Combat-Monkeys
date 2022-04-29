50f31c305bb2bea080da66b131f30206107e0063
Juandiem <juandiem@ucm.es>
2022-04-29 18:12:01 +0200

    Cambio en el bat de build_engine

--- build_engine.bat
+++ build_engine.bat
@@ -4,13 +4,14 @@ title Game_Engine_Build
 
 :: Release compiling boolean
 set RELEASE_GAME=false
-set GAME_WORKING_DIR=.\
+set DEVELOPMENT=false
+
 :: Directory variables
-set ENGINE_SOL_DIR=%GAME_WORKING_DIR%\engine\K_Engine.sln
-set ENGINE_SOURCE_FILE=%GAME_WORKING_DIR%\engine\src\engine_prj\Engine.cpp
+set ENGINE_SOL_DIR=%cd%\engine\K_Engine.sln
+set ENGINE_SOURCE_FILE=%cd%\engine\src\engine_prj\Engine.cpp
 
 :: Change DEVELOPMENT macro
-powershell -Command "(gc %ENGINE_SOURCE_FILE%) -replace '#define DEVELOPMENT', '//#define DEVELOPMENT' | Out-File -encoding ASCII %ENGINE_SOURCE_FILE%"
+if %DEVELOPMENT% == false powershell -Command "(gc %ENGINE_SOURCE_FILE%) -replace '#define DEVELOPMENT', '//#define DEVELOPMENT' | Out-File -encoding ASCII %ENGINE_SOURCE_FILE%"
 
 :: Compiling of game dll and movement to their folders 
 :: (this should be moved to the main build eventually)
