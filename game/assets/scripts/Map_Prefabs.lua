8b41f5dcfda2cada05f20c315bc4d3557798f829
adriac02 <adriac02@ucm.es>
2022-06-20 14:47:04 +0200

    Script de Lua lee y crea personajes desde txt

--- game/assets/scripts/Map_Prefabs.lua
+++ game/assets/scripts/Map_Prefabs.lua
@@ -242,6 +242,282 @@ Map_Prefabs_prefabs = {
         MeshRenderer = {
             MeshFile = "Pedro.mesh",
         }
+    },
+    Armature = {
+        Transform = {
+            position = " -147.764664 ,100.264107 ,0.000013 ",
+            rotation = " 0 ,0 ,0 ",
+            scale = " 1.000000 ,1.000000 ,1.000000 ",
+        },
+        Animator = {
+            defaultAnim = "None",
+             startsEnabled = "false"
+        },
+        WeaponStack = {
+            
+        },
+        Enabled = "true",
+        Controller = {
+            EnableOnStart = "false"
+        },
+        RigidBody = {
+            Type = "Dynamic" ,
+            Collider = "Box" ,
+            Mass = "1.0" ,
+            Dimensions = "8,  16,  8",
+            friction = "0.1",
+             restitution = "0.0",
+            group = "monos",
+              mask = " suelo, armas, explosion, oil",
+            offsetCenter = "0, 8, 0",
+            isTrigger = "false" ,
+            isEnabled = "true"
+        },
+        Health = {
+            
+        },
+        PlayerInfo = {
+            Team = "0",
+             Order = "1"
+        },
+        MeshRenderer = {
+            MeshFile = "Generic.mesh",
+        }
+    },
+    Armature001 = {
+        Transform = {
+            position = " 171.718582 ,100.814651 ,0.000014 ",
+            rotation = " 0 ,0 ,0 ",
+            scale = " 1.000000 ,1.000000 ,1.000000 ",
+        },
+        Animator = {
+            defaultAnim = "None",
+             startsEnabled = "false"
+        },
+        WeaponStack = {
+            
+        },
+        Enabled = "true",
+        Controller = {
+            EnableOnStart = "false"
+        },
+        RigidBody = {
+            Type = "Dynamic" ,
+            Collider = "Box" ,
+            Mass = "1.0" ,
+            Dimensions = "8,  16,  8",
+            friction = "0.1",
+             restitution = "0.0",
+            group = "monos",
+              mask = " suelo, armas, explosion, oil",
+            offsetCenter = "0, 8, 0",
+            isTrigger = "false" ,
+            isEnabled = "true"
+        },
+        Health = {
+            
+        },
+        PlayerInfo = {
+            Team = "1",
+             Order = "2"
+        },
+        MeshRenderer = {
+            MeshFile = "Generic_.mesh",
+        }
+    },
+    Armature002 = {
+        Transform = {
+            position = " 64.009254 ,52.118889 ,0.000007 ",
+            rotation = " 0 ,0 ,0 ",
+            scale = " 1.000000 ,1.000000 ,1.000000 ",
+        },
+        Animator = {
+            defaultAnim = "None",
+             startsEnabled = "false"
+        },
+        WeaponStack = {
+            
+        },
+        Enabled = "true",
+        Controller = {
+            EnableOnStart = "false"
+        },
+        RigidBody = {
+            Type = "Dynamic" ,
+            Collider = "Box" ,
+            Mass = "1.0" ,
+            Dimensions = "8,  16,  8",
+            friction = "0.1",
+             restitution = "0.0",
+            group = "monos",
+              mask = " suelo, armas, explosion, oil",
+            offsetCenter = "0, 8, 0",
+            isTrigger = "false" ,
+            isEnabled = "true"
+        },
+        Health = {
+            
+        },
+        PlayerInfo = {
+            Team = "1",
+             Order = "3"
+        },
+        MeshRenderer = {
+            MeshFile = "Generic_.mesh",
+        }
+    },
+    Armature003 = {
+        Transform = {
+            position = " 113.182671 ,110.748940 ,0.000015 ",
+            rotation = " 0 ,0 ,0 ",
+            scale = " 1.000000 ,1.000000 ,1.000000 ",
+        },
+        Animator = {
+            defaultAnim = "None",
+             startsEnabled = "false"
+        },
+        WeaponStack = {
+            
+        },
+        Enabled = "true",
+        Controller = {
+            EnableOnStart = "false"
+        },
+        RigidBody = {
+            Type = "Dynamic" ,
+            Collider = "Box" ,
+            Mass = "1.0" ,
+            Dimensions = "8,  16,  8",
+            friction = "0.1",
+             restitution = "0.0",
+            group = "monos",
+              mask = " suelo, armas, explosion, oil",
+            offsetCenter = "0, 8, 0",
+            isTrigger = "false" ,
+            isEnabled = "true"
+        },
+        Health = {
+            
+        },
+        PlayerInfo = {
+            Team = "1",
+             Order = "1"
+        },
+        MeshRenderer = {
+            MeshFile = "Generic_.mesh",
+        }
+    },
+    Armature004 = {
+        Transform = {
+            position = " -79.880348 ,110.198395 ,0.000015 ",
+            rotation = " 0 ,0 ,0 ",
+            scale = " 1.000000 ,1.000000 ,1.000000 ",
+        },
+        Animator = {
+            defaultAnim = "None",
+             startsEnabled = "false"
+        },
+        WeaponStack = {
+            
+        },
+        Enabled = "true",
+        Controller = {
+            EnableOnStart = "false"
+        },
+        RigidBody = {
+            Type = "Dynamic" ,
+            Collider = "Box" ,
+            Mass = "1.0" ,
+            Dimensions = "8,  16,  8",
+            friction = "0.1",
+             restitution = "0.0",
+            group = "monos",
+              mask = " suelo, armas, explosion, oil",
+            offsetCenter = "0, 8, 0",
+            isTrigger = "false" ,
+            isEnabled = "true"
+        },
+        Health = {
+            
+        },
+        PlayerInfo = {
+            Team = "0",
+             Order = "2"
+        },
+        MeshRenderer = {
+            MeshFile = "Generic.mesh",
+        }
+    },
+    Armature005 = {
+        Transform = {
+            position = " -84.847488 ,40.148918 ,0.000005 ",
+            rotation = " 0 ,0 ,0 ",
+            scale = " 1.000000 ,1.000000 ,1.000000 ",
+        },
+        Animator = {
+            defaultAnim = "None",
+             startsEnabled = "false"
+        },
+        WeaponStack = {
+            
+        },
+        Enabled = "true",
+        Controller = {
+            EnableOnStart = "false"
+        },
+        RigidBody = {
+            Type = "Dynamic" ,
+            Collider = "Box" ,
+            Mass = "1.0" ,
+            Dimensions = "8,  16,  8",
+            friction = "0.1",
+             restitution = "0.0",
+            group = "monos",
+              mask = " suelo, armas, explosion, oil",
+            offsetCenter = "0, 8, 0",
+            isTrigger = "false" ,
+            isEnabled = "true"
+        },
+        Health = {
+            
+        },
+        PlayerInfo = {
+            Team = "0",
+             Order = "3"
+        },
+        MeshRenderer = {
+            MeshFile = "Generic.mesh",
+        }
+    },
+    OilArmature = {
+        Transform = {
+            position = " 0.000000 ,0.000000 ,-0.000000 ",
+            rotation = " 0 ,0 ,0 ",
+            scale = " 1.000000 ,1.000000 ,1.000000 ",
+        },
+        RigidBody = {
+            Type = "Static" ,
+            Collider = "Box" ,
+            Mass = "1.0" ,
+            Dimensions = "1000,  870,  1000",
+            friction = "0.8",
+             restitution = "0.0",
+            group = "oil",
+              mask = "monos, armas",
+            offsetCenter = "0, -435, 0",
+            isTrigger = "true" ,
+            isEnabled = "true"
+        },
+        Animator = {
+            defaultAnim = "None",
+             startsEnabled = "false"
+        },
+        FryingOil = {
+            
+        },
+        MeshRenderer = {
+            MeshFile = "Oil.mesh",
+        }
     }
 }
 
@@ -250,16 +526,18 @@ function popSceneGame()
     sceneMan:popScene("menu");
 end
 
-local index = 15;
+local index = 13;
 Map_Prefabs_entities = {}
 
-Map_Prefabs_entities = {"Settings 0 0 0", "MainLight 0 0 0", "time 0 0 0", "health 0 0 0", "backgroundHealth 0 0 0", "mainMenuTextGame 0 0 0", "mainMenuButton 0 0 0","Avion 0 5000 0","Avion 0 5000 0","Avion 0 5000 0","Avion 0 5000 0","Avion 0 5000 0", "ArmatureDario001 0 200 0", "PedroArmature 2000 200 0"}
-local file = io.open("assets\\scripts\\testMapPrefabs.txt", "rb")
+Map_Prefabs_entities = {"Settings 0 0 0", "MainLight 0 2000 2000", "time 50 5 1000", "health 60 -3 1000","backgroundHealth 60 0 5", "mainMenuTextGame 10 3 1200",
+"mainMenuButton 0 0 100","Avion 0 5000 0","Avion 0 5000 0","Avion 0 5000 0","Avion 0 5000 0","Avion 0 5000 0"}
+
+local file = io.open("assets\\maps\\testMapPrefabs.txt", "rb")
 
     if not file then return nil end
     
     local lines = {}
-    for line in io.lines("assets\\scripts\\testMapPrefabs.txt") do
+    for line in io.lines("assets\\maps\\testMapPrefabs.txt") do
         local nums = {}
         for n in line:gmatch("%w+") do
             table.insert(nums,n)
@@ -268,12 +546,37 @@ local file = io.open("assets\\scripts\\testMapPrefabs.txt", "rb")
     end
     
     file:close()
+
+    -- Players from 1 to 4 are the first team and 5 to 8 second team
     local coorY = 0
     for x, val in ipairs(lines) do
         local coorX = 0
         for y, val2 in ipairs(lines[x]) do
             if val2 == "1" then
-                print(coorX)
+                Map_Prefabs_entities[index] = "PedroArmature "..coorX.." "..coorY.." 0"
+                index = index + 1
+            elseif val2 == "2" then
+                Map_Prefabs_entities[index] = "Armature "..coorX.." "..coorY.." 0"
+                index = index + 1
+            elseif val2 == "3" then
+                Map_Prefabs_entities[index] = "Armature004 "..coorX.." "..coorY.." 0"
+                index = index + 1
+            elseif val2 == "4" then
+                Map_Prefabs_entities[index] = "Armature005 "..coorX.." "..coorY.." 0"
+                index = index + 1
+            elseif val2 == "5" then
+                Map_Prefabs_entities[index] = "ArmatureDario001 "..coorX.." "..coorY.." 0"
+                index = index + 1
+            elseif val2 == "6" then
+                Map_Prefabs_entities[index] = "Armature003 "..coorX.." "..coorY.." 0"
+                index = index + 1
+            elseif val2 == "7" then
+                Map_Prefabs_entities[index] = "Armature001 "..coorX.." "..coorY.." 0"
+                index = index + 1
+            elseif val2 == "8" then
+                Map_Prefabs_entities[index] = "Armature002 "..coorX.." "..coorY.." 0"
+                index = index + 1
+            elseif val2 == "X" then
                 Map_Prefabs_entities[index] = "Cube "..coorX.." "..coorY.." 0"
                 index = index + 1
             end
@@ -281,5 +584,6 @@ local file = io.open("assets\\scripts\\testMapPrefabs.txt", "rb")
         end
         coorY = coorY - 1000
     end
+    Map_Prefabs_entities[index] = "OilArmature 0 "..coorY.." 0"
 
 
